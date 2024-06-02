af434409f0664fcbbd68a3e336b64d6ff4fb661e
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-06-02 19:10:44 -0300

    se mejora estilo hardcodeado de hard skills

--- src/pages/WebDeveloper.jsx
+++ src/pages/WebDeveloper.jsx
@@ -1,28 +1,23 @@
 import { ProgressBar } from "react-bootstrap";
 
 const WebDeveloper = () => {
-  const now = 60;
+  const now = 80;
   return (
     <div className="container h-screen">
       <h4>Hard Skills</h4>
 
       <div className="container  flex items-center ">
-        <div className=" bg-warning-200 flex ">
+        <div className=" bg-warning-200 flex  rounded-md">
           <div>
-            <h6 className="p-1">html </h6>
-            <h6 className="p-1">nivel</h6>
+            <img
+              className="max-w-12 m-2"
+              src="https://firebasestorage.googleapis.com/v0/b/portfolio-imagenes.appspot.com/o/logos%2Flogo-javascript.svg?alt=media&token=6ad8df87-e2e0-476a-b669-bc005ca45279"
+              alt=""
+            />
           </div>
           <ProgressBar now={now} label={`${now}%`} className="w-48 m-4  " />
+          <h6 className=" m-2">nivel</h6>
         </div>
-      </div>
-      <div>
-        <ProgressBar now={now} label={`${now}%`} className="w-48 m-4" />
-      </div>
-      <div>
-        <ProgressBar now={now} label={`${now}%`} className="w-48 m-4" />
-      </div>
-      <div>
-        <ProgressBar now={now} label={`${now}%`} className="w-48 m-4" />
       </div>
     </div>
   );
