57d2cb3dc0f705dd6ba44bc5c82d1a2e355f2812
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-06-02 16:04:04 -0300

    se acomoda progress Bar

--- src/pages/WebDeveloper.jsx
+++ src/pages/WebDeveloper.jsx
@@ -1,7 +1,29 @@
+import { ProgressBar } from "react-bootstrap";
+
 const WebDeveloper = () => {
+  const now = 60;
   return (
-    <div className="container">
-      <p className="text-green-400">web developer works!</p>
+    <div className="container h-screen">
+      <h4>Hard Skills</h4>
+
+      <div className="container  flex items-center ">
+        <div className=" bg-warning-200 flex ">
+          <div>
+            <h6 className="p-1">html </h6>
+            <h6 className="p-1">nivel</h6>
+          </div>
+          <ProgressBar now={now} label={`${now}%`} className="w-48 m-4  " />
+        </div>
+      </div>
+      <div>
+        <ProgressBar now={now} label={`${now}%`} className="w-48 m-4" />
+      </div>
+      <div>
+        <ProgressBar now={now} label={`${now}%`} className="w-48 m-4" />
+      </div>
+      <div>
+        <ProgressBar now={now} label={`${now}%`} className="w-48 m-4" />
+      </div>
     </div>
   );
 };
