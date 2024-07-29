0d0699c04b5120aa76022e882d2e3f66330b9ac1
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-07-29 13:29:27 -0300

    se agrega links de linkdin y github a footer y nav

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,24 @@
 const Footer = () => {
   return (
-    <div className="   bg-slate-600 min-w-screen min-h-20 ">
-      <div className="">
-        <h1>Footer</h1>
-      </div>
+    <div className="   bg-slate-600 min-w-screen min-h-20 flex flex-col justify-start">
+      <a href="https://github.com/marguigle?tab=repositories" target="_blank">
+        <img
+          src="logotipo-de-github.png"
+          alt="logotipo degithub"
+          className="logo w-16 h-16 p-3"
+        />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/marcelo-iglesia-3047b98b/"
+        target="_blank"
+      >
+        {" "}
+        <img
+          src="linkedin.png"
+          alt="logotipo linkedin"
+          className="logo w-16 h-16 p-3"
+        />
+      </a>
     </div>
   );
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -66,6 +66,26 @@ const Navbar = () => {
           </button>
         </ul>
       </div>
+      <div className="flex">
+        <a href="https://github.com/marguigle?tab=repositories" target="_blank">
+          <img
+            src="logotipo-de-github.png"
+            alt="logotipo degithub"
+            className="logo w-16 h-16 p-3"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/marcelo-iglesia-3047b98b/"
+          target="_blank"
+        >
+          {" "}
+          <img
+            src="linkedin.png"
+            alt="logotipo linkedin"
+            className="logo w-16 h-16 p-3"
+          />
+        </a>
+      </div>
     </nav>
   );
 };
