af53693d6987219f715c96bff1db92cd18abb501
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-08-17 23:34:32 -0300

    se agrega blur a soft skills

--- src/components/SoftSkills.jsx
+++ src/components/SoftSkills.jsx
@@ -10,14 +10,14 @@ const SoftSkills = () => {
     { id: 8, skill: "Colaboración y cooperación" },
   ];
   return (
-    <div className=" flex  bg-[url(https://firebasestorage.googleapis.com/v0/b/portfolio-imagenes.appspot.com/o/mountains-8347890_1920.jpg?alt=media&token=db4f74ab-861b-4368-8053-4a0860ef76a1)] bg-cover bg-center h-screen  from-stone-600  justify-center ">
-      <div className="text-2xl  mb-5  tems-center justify-center ">
+    <div className=" flex  bg-[url(https://firebasestorage.googleapis.com/v0/b/portfolio-imagenes.appspot.com/o/mountains-8347890_1920.jpg?alt=media&token=db4f74ab-861b-4368-8053-4a0860ef76a1)] bg-cover bg-center h-screen  from-stone-600  justify-center  ">
+      <div className="text-2xl  mb-5  tems-center justify-center  ">
         <h5 className="pt-24 m-2 text-white">Soft skills</h5>
       </div>
-      <ul className="w-56 text-white text-center m-4">
+      <ul className="w-56 text-white text-center m-4  ">
         {softSk.map((skill) => (
           <li
-            className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 rk:border-opacity-50"
+            className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 rk:border-opacity-50 bg-white bg-opacity-10 backdrop-blur-md rounded-md"
             key={skill.id}
           >
             {skill.skill}
